3b3f035ba0e318fbe4eae2a3158d514f58f7f2ba
Petrosz007 <andipeti2000@gmail.com>
2021-11-28 23:50:38 +0100

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ManualTester from './components/ManualTester';
 
+import './App.scss';
+
 function App() {
-  return <ManualTester />;
+  return (
+    <>
+      <a
+        href="https://test-design.org/practical-exercises/"
+        className="backLink"
+      >
+        Back to Test Design Exercises
+      </a>
+      <ManualTester />
+    </>
+  );
 }
 
 export default App;

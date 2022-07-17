f4dc87f78e2436f7b111f09bbdc1ef4df78cf9ee
Andi Péter <andipeti2000@gmail.com>
2022-07-17 13:33:43 +0200

    Update nx version

--- libs/gpt-algorithm/jest.config.ts
+++ libs/gpt-algorithm/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'gpt-algorithm',
   preset: '../../jest.preset.js',
   globals: {
--- libs/gpt-common/jest.config.ts
+++ libs/gpt-common/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'gpt-common',
   preset: '../../jest.preset.js',
   globals: {
